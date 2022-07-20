3cdb83c945df00ea879c4accd5aefc71e782f151
Palskie <57343545+plskz@users.noreply.github.com>
2022-07-20 09:08:56 +0000

    feat: add discord status

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,33 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useLanyard } from 'use-lanyard';
 import Socials from './components/Socials';
-
-interface User {
-  id: string;
-  username: string;
-  avatar: string;
-  avatar_decoration: string | null;
-  discriminator: string;
-  public_flags: number;
-  flags: number;
-  banner: string;
-  banner_color: string | null;
-  accent_color: string | null;
-  bio: string;
-}
 
 const discordID = '90431685472038912';
 
 const App = () => {
-  const [data, setData] = useState<User>();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    axios.get(`https://dcdn.dstn.to/profile/${discordID}`).then((res) => {
-      const data: User = res.data.user;
-      setData(data);
-      setLoading(false);
-    });
-  }, []);
+  const { data: activity } = useLanyard(discordID);
 
   return (
     <div className='flex items-center justify-center h-screen'>
       <div className='card w-[32rem] bg-base-100 shadow-2xl'>
         <figure className='px-10 pt-10'>
-          {loading ? (
-            <div className='avatar animate-pulse'>
-              <div className=' bg-gray-300 w-32 h-32 mask mask-decagon' />
+          {!activity ? (
+            <div className='avatar animate-pulse indicator'>
+              <span className='indicator-item indicator-bottom badge right-5 bottom-6 w-7 h-7 border-4 border-base-100 bg-grey-600' />
+              <div className=' bg-gray-300 w-32 h-32 mask mask-hexagon-2' />
             </div>
           ) : (
-            <div className='avatar'>
+            <div className='avatar indicator'>
+              <span
+                className={`indicator-item indicator-bottom badge right-5 bottom-6 w-7 h-7 border-4 border-base-100 ${
+                  activity.discord_status === 'offline'
+                    ? 'bg-grey-600'
+                    : 'bg-green-600'
+                }`}
+              />
               <img
-                className='mask mask-decagon'
-                src={`https://cdn.discordapp.com/avatars/${discordID}/${data?.avatar}`}
-                alt={data?.username}
+                className='mask mask-hexagon-2'
+                src={`https://cdn.discordapp.com/avatars/${discordID}/${activity.discord_user.avatar}`}
+                alt={activity.discord_user.username}
               />
             </div>
           )}
@@ -52,7 +36,7 @@ const App = () => {
           <h2 className='card-title text-4xl text-sky-400/100 lg:font-extrabold lg:text-transparent lg:bg-clip-text lg:bg-gradient-to-r lg:from-purple-500 lg:to-pink-700'>
             Palskie
           </h2>
-          <p>{data?.bio}</p>
+          <p>Chaeyoung</p>
           <div className='w-full h-1/4 mt-5 lg:mt-3 md:mt-3'>
             <Socials />
           </div>
