437fc1894092e64a8403722cb097b75fa092fecd
Zai Santillan <zaisantillan14@gmail.com>
2022-10-26 02:55:55 +0800

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "@styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
 };
 
 export default MyApp;
